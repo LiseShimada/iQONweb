683e1b1b707c62616438f801c5897596ae50ce6f
Rise <rixholy22@gmail.com>
2014-12-16 19:36:01 +0900

    #6 fixed

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,5 +6,12 @@
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
 
-User.create(name: "example", email: "", password: "")
-Post.create(date: "2000-01-01", subject: "0", title: "example", comment: "example", picture: "iQ_1.jpg")
+User.create(name: "example1", email: "example1@ex.com", password: "example1")
+User.create(name: "example2", email: "example2@ex.com", password: "example2")
+User.create(name: "example3", email: "example3@ex.com", password: "example3")
+
+Post.create(date: "2000-01-01", subject: "1", title: "example1", comment: "example1", picture: "iQ_1.jpg")
+Post.create(date: "2000-01-01", subject: "2", title: "example2", comment: "example2", picture: "iQ_2.jpg")
+Post.create(date: "2000-01-01", subject: "3", title: "example3", comment: "example3", picture: "iQ_3.jpg")
+Post.create(date: "2000-01-01", subject: "4", title: "example4", comment: "example4", picture: "iQ_4.jpg")
+Post.create(date: "2000-01-01", subject: "5", title: "example5", comment: "example5", picture: "iQ_5.jpg")
